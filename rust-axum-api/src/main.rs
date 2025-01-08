63ed2b7052a613d3038ed7bc2040d016f4c5965d
塚田 聖也 <seiya.tsukada@mac.lan>
2025-01-08 17:40:37 +0900

    add obtain query paramter

--- rust-axum-api/src/main.rs
+++ rust-axum-api/src/main.rs
@@ -1,11 +1,18 @@
 use axum::{
     routing::get,
     Router,
-    extract::Path
+    extract::{Path, Query},
 };
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
+
+    #[derive(Deserialize)]
+    struct Params {
+        id: Option<u8>,
+    }
+
     // ハンドラーを定義
     async fn root_handler() -> String {
         "Hello World".to_string()
@@ -16,14 +23,20 @@ async fn main() {
     }
 
     async fn item_id_handler(Path(id): Path<String>) -> String {
-        format!("item id:{}", id)
+        format!("item id is {}", id)
+    }
+
+    async fn get_query_handler(Query(params): Query<Params>) -> String {
+        let id = params.id.unwrap_or(0);
+        format!("id is {}", id)
     }
 
     // ルートを定義
     let app = Router::new()
         .route("/", get(root_handler))
         .route("/item", get(item_handler))
-        .route("/item/:id", get(item_id_handler));
+        .route("/item/:id", get(item_id_handler))
+        .route("/query", get(get_query_handler));
 
     // 指定したポートにサーバを開く
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
